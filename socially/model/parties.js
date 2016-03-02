313ada8e97b8e77ca4c03c72f960092344808019
chenxinji <695881071@qq.com>
2016-03-02 12:50:14 -0500

    updated the publish module to server folder

--- socially/model/parties.js
+++ socially/model/parties.js
@@ -1,140 +1,101 @@
-/**
- * Created by chenxin on 2/25/2016.
- */
 Parties = new Mongo.Collection("parties");
 
 Parties.allow({
-    insert: function (userId, party) {
-        return userId && party.owner === userId;
-    },
-    update: function (userId, party, fields, modifier) {
-        return userId && party.owner === userId;
-    },
-    remove: function (userId, party) {
-        return userId && party.owner === userId;
-    }
+  insert: function (userId, party) {
+    return userId && party.owner === userId;
+  },
+  update: function (userId, party, fields, modifier) {
+    return userId && party.owner === userId;
+  },
+  remove: function (userId, party) {
+    return userId && party.owner === userId;
+  }
 });
 
 var getContactEmail = function (user) {
-    if (user.emails && user.emails.length)
-        return user.emails[0].address;
+  if (user.emails && user.emails.length)
+    return user.emails[0].address;
 
-    if (user.services && user.services.facebook && user.services.facebook.email)
-        return user.services.facebook.email;
+  if (user.services && user.services.facebook && user.services.facebook.email)
+    return user.services.facebook.email;
 
-    return null;
+  return null;
 };
 
 Meteor.methods({
-    invite: function (partyId, userId) {
-        check(partyId, String);
-        check(userId, String);
+  invite: function (partyId, userId) {
+    check(partyId, String);
+    check(userId, String);
 
-        var party = Parties.findOne(partyId);
+    var party = Parties.findOne(partyId);
 
-        if (!party)
-            throw new Meteor.Error(404, "No such party!");
+    if (!party)
+      throw new Meteor.Error(404, "No such party!");
 
-        if (party.owner !== this.userId)
-            throw new Meteor.Error(404, "No permissions!");
+    if (party.owner !== this.userId)
+      throw new Meteor.Error(404, "No permissions!");
 
-        if (party.public)
-            throw new Meteor.Error(400, "That party is public. No need to invite people.");
+    if (party.public)
+      throw new Meteor.Error(400, "That party is public. No need to invite people.");
 
-        if (userId !== party.owner && !_.contains(party.invited, userId)) {
-            Parties.update(partyId, {$addToSet: {invited: userId}});
+    if (userId !== party.owner && !_.contains(party.invited, userId)) {
+      Parties.update(partyId, {$addToSet: {invited: userId}});
 
-            var from = getContactEmail(Meteor.users.findOne(this.userId));
-            var to = getContactEmail(Meteor.users.findOne(userId));
+      var from = getContactEmail(Meteor.users.findOne(this.userId));
+      var to = getContactEmail(Meteor.users.findOne(userId));
 
-            if (Meteor.isServer && to) {
-                Email.send({
-                    from: "noreply@socially.com",
-                    to: to,
-                    replyTo: from || undefined,
-                    subject: "PARTY: " + party.title,
-                    text: "Hey, I just invited you to '" + party.title + "' on Socially." +
-                    "\n\nCome check it out: " + Meteor.absoluteUrl() + "\n"
-                });
-            }
-        }
-    },
-    rsvp: function (partyId, rsvp) {
-        check(partyId, String);
-        check(rsvp, String);
-
-        if (!this.userId)
-            throw new Meteor.Error(403, "You must be logged in to RSVP");
-
-        if (!_.contains(['yes', 'no', 'maybe'], rsvp))
-            throw new Meteor.Error(400, "Invalid RSVP");
-
-        var party = Parties.findOne(partyId);
-
-        if (!party)
-            throw new Meteor.Error(404, "No such party");
-
-        if (!party.public && party.owner !== this.userId && !_.contains(party.invited, this.userId))
-            throw new Meteor.Error(403, "No such party"); // its private, but let's not tell this to the user
-
-        var rsvpIndex = _.indexOf(_.pluck(party.rsvps, 'user'), this.userId);
-
-        if (rsvpIndex !== -1) {
-            // update existing rsvp entry
-            if (Meteor.isServer) {
-                // update the appropriate rsvp entry with $
-                Parties.update(
-                    {_id: partyId, "rsvps.user": this.userId},
-                    {$set: {"rsvps.$.rsvp": rsvp}});
-            } else {
-                // minimongo doesn't yet support $ in modifier. as a temporary
-                // workaround, make a modifier that uses an index. this is
-                // safe on the client since there's only one thread.
-                var modifier = {$set: {}};
-                modifier.$set["rsvps." + rsvpIndex + ".rsvp"] = rsvp;
-
-                Parties.update(partyId, modifier);
-            }
-        } else {
-            // add new rsvp entry
-            Parties.update(partyId,
-                {$push: {rsvps: {user: this.userId, rsvp: rsvp}}});
-        }
+      if (Meteor.isServer && to) {
+        Email.send({
+          from: "noreply@socially.com",
+          to: to,
+          replyTo: from || undefined,
+          subject: "PARTY: " + party.title,
+          text: "Hey, I just invited you to '" + party.title + "' on Socially." +
+          "\n\nCome check it out: " + Meteor.absoluteUrl() + "\n"
+        });
+      }
     }
-});
+  },
+  rsvp: function (partyId, rsvp) {
+    check(partyId, String);
+    check(rsvp, String);
 
-Meteor.publish("parties", function (options,searchString) {
-    if (!searchString || searchString == null) {
-        searchString = '';
+    if (!this.userId)
+      throw new Meteor.Error(403, "You must be logged in to RSVP");
+
+    if (!_.contains(['yes', 'no', 'maybe'], rsvp))
+      throw new Meteor.Error(400, "Invalid RSVP");
+
+    var party = Parties.findOne(partyId);
+
+    if (!party)
+      throw new Meteor.Error(404, "No such party");
+
+    if (!party.public && party.owner !== this.userId && !_.contains(party.invited, this.userId))
+      throw new Meteor.Error(403, "No such party"); // its private, but let's not tell this to the user
+
+    var rsvpIndex = _.indexOf(_.pluck(party.rsvps, 'user'), this.userId);
+
+    if (rsvpIndex !== -1) {
+      // update existing rsvp entry
+      if (Meteor.isServer) {
+        // update the appropriate rsvp entry with $
+        Parties.update(
+          {_id: partyId, "rsvps.user": this.userId},
+          {$set: {"rsvps.$.rsvp": rsvp}});
+      } else {
+        // minimongo doesn't yet support $ in modifier. as a temporary
+        // workaround, make a modifier that uses an index. this is
+        // safe on the client since there's only one thread.
+        var modifier = {$set: {}};
+        modifier.$set["rsvps." + rsvpIndex + ".rsvp"] = rsvp;
+
+        Parties.update(partyId, modifier);
+      }
+    } else {
+      // add new rsvp entry
+      Parties.update(partyId,
+        {$push: {rsvps: {user: this.userId, rsvp: rsvp}}});
     }
-
-    //return Parties.find({
-    selector = {
-        name: { '$regex' : '.*' + searchString || '' + '.*', '$options' : 'i' },
-
-        $or: [
-            {
-                $and: [
-                    {"public": true},
-                    {"public": {$exists: true}}
-                ]
-            },
-            {
-                $and: [
-                    {owner: this.userId},
-                    {owner: {$exists: true}}
-                ]
-            },
-            {
-                $and: [
-                    {invited: this.userId},
-                    {invited: {$exists: true}}
-                ]
-            }
-        ]
-    //},options);
-    };
-
-    Counts.publish(this, 'numberOfParties', Parties.find(selector), {noReady: true});
-    return Parties.find(selector, options);
+  }
 });
